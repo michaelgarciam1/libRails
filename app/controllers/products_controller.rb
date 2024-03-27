1aa65584f78e63bcfa73a45b90cf311815ea6497
Michael <michaelgarciamoreiraa@gmail.com>
2024-03-27 11:58:48 +0100

    tests

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,14 +22,14 @@ class ProductsController < ApplicationController
   # POST /products or /products.json
   def create
     @product = Product.new(product_params)
-
     respond_to do |format|
       if @product.save
-        format.html { redirect_to product_url(@product), notice: "Product was successfully created." }
-        format.json { render :show, status: :created, location: @product }
+        format.html{redirect_to @product, notice: "Product was successfully created."}
+        format.json{render :show, status: :created, location: @product}
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
+        puts @product.errors.full_messages
+        format.html{render :new, status: :unprocessable_entity}
+        format.json{render json: @product.errors, status: :unprocessable_entity}
       end
     end
   end
